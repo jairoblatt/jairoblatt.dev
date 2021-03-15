b12997ea9b20c2719e788e38db6975285e8047f2
jairoblatt <jairoblattc@gmail.com>
2021-03-15 19:56:32 -0300

    add: Seo meta tags in blog page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -3,10 +3,8 @@
     <BlogpostItem :post="post" />
   </section>
 </template>
-<script lang="ts">
-import Vue from 'vue';
-
-export default Vue.extend({
+<script>
+export default {
   components: {
     BlogpostItem: () => import('@/components/templates/blog/BlogpostItem.vue'),
   },
@@ -23,7 +21,58 @@ export default Vue.extend({
       post,
     };
   },
-});
+
+  head() {
+    return {
+      title: this.post.title,
+      titleTemplate: '%s - Jairo Blatt',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.post.description,
+        },
+
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.post.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.post.description,
+        },
+        { hid: 'og:type', property: 'og:type', content: 'article' },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: `https://nuxtjs.org/blog/${this.post.slug}`,
+        },
+        { hid: 'og:image', property: 'og:image', content: this.post.imgUrl },
+
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.post.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.post.description,
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: this.post.imgUrl,
+        },
+        {
+          hid: 'twitter:image:',
+          name: 'twitter:image:alt',
+          content: 'Blog post image',
+        },
+      ],
+    };
+  },
+};
 </script>
 <style lang="postcss" scoped>
 .blog-item__container {
