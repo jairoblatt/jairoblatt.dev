9825ee2777c8066b90aee3cc9c2e3305ba3a4b45
jairoblatt <jairoblattc@gmail.com>
2021-03-18 09:20:51 -0300

    add: sortBy title asc

--- pages/projects.vue
+++ pages/projects.vue
@@ -40,7 +40,7 @@ export default Vue.extend({
   async fetch() {
     try {
       const path = `projects/${this.$i18n.locale}`;
-      const projects = await this.$content(path).fetch();
+      const projects = await this.$content(path).sortBy('title', 'asc').fetch();
       this.projects = projects || [];
     } catch (e) {
       console.error(e);
