502064379d07c7bb5742d0b3471af4f3d7d24dbd
jairoblatt <jairoblattc@gmail.com>
2021-03-28 15:49:23 -0300

    Privacy policy translate

--- lang/en.js
+++ lang/en.js
@@ -28,4 +28,77 @@ module.exports = {
   home: {
     latestPosts: 'Latest Posts:',
   },
+
+  policies: {
+    privacyPolicy: {
+      title: 'Privacy policy for jairoblatt.dev',
+      textTemplate: `<p>All of your personal information collected will be used
+      to help make your visit to this site as productive and enjoyable as possible.</p>
+      <br />
+      <p>The guarantee of the confidentiality of the personal data of the users of this
+      website is important for the<span class="policies__container-text--highlight">jairoblatt.dev.</span>
+      </p><br/><p>All personal information using the
+      <span class="policies__container-text--highlight">jairoblatt.dev </span>
+     will be dealt with in accordance with Law No. 13,709, of August 14,
+      2018.</p>
+    <br/>
+    <p> The personal information collected may include your name, e-mail, telephone and / or mobile phone number, address,
+    date of birth and / or others.</p> <br />
+    <p>The use of <span class="policies__container-text--highlight">jairoblatt.dev </span>
+      assumes acceptance of this privacy agreement. The website jairoblatt.dev
+      reserves the right to change this agreement without prior notice. This way,
+      we recommend that you check our privacy policy regularly
+      in order to be always up to date.</p>`,
+    },
+
+    cookies: {
+      title: 'Cookies and Web Beacons',
+      textTemplate: `<p> We use cookies to store information, such as
+      like your personal preferences when you visit our website. How
+      for example to save your theme preferences when you return to the site</p>
+      <p> The Application uses Google Analytics, an analytics service
+      provided by Google, Inc. ("Google"). Google Analytics uses cookies
+      to help us analyze how Users use the Application. The informations
+      collected in the cookie about how you use the Application (including
+      your IP address) will be transmitted to and stored by Google in
+      servers in the United States. Google will use this information with the
+      purpose of evaluating how you use the Application, creating
+      reports on the activities on the App and provide other services
+      relating to activities on the Application and Internet usage. Google
+      You can also transfer this information to third parties when required by
+      law or for such third parties to process the information in the name and for
+      Google account. Google will not associate your IP address with any other
+      given away. Google's use of this data is governed by the Terms of Use
+      Google Analytics and
+      <a class="policies__container-text--highlight"> Privacy Policy
+      Of google.</a></p> <br />
+    <p> In addition, we also use third party advertising on our website to
+      support maintenance costs. Some of these advertisers may
+      use technologies such as cookies and / or web beacons when advertising on
+      our website, which will make these advertisers (like Google
+      through Google AdSense) also receive your personal information, such as the
+      IP address, your ISP, your browser, etc. This function is usually
+      used for geotargeting (show São Paulo advertising only to
+      readers from São Paulo, for example) or display advertising
+      targeted to a type of user (such as showing advertising from
+      restaurant to a user who regularly visits cooking sites,
+      for example.).</p><br />
+    <p> You have the power to turn off your cookies, in your browser options.
+      However, this may change the way you interact with our website,
+      or other websites. This may affect or not allow you to log in to
+      internet sites.</p>`,
+    },
+
+    thirdPartySites: {
+      title: 'Third party website',
+      textTemplate: `<p> The <span class="policies__container-text--highlight">jairoblatt.dev</span>
+      has links to other sites, which may contain useful information. THE
+      our privacy policy does not apply to third party websites, so
+      that if you visit another site from ours you should read the policy of
+      privacy. Google's Privacy Policy.
+    </p><br />
+    <p>We are not responsible for the privacy policy or content
+      present on those same sites. </p>`,
+    },
+  },
 };
--- lang/pt-BR.js
+++ lang/pt-BR.js
@@ -28,4 +28,78 @@ module.exports = {
   home: {
     latestPosts: 'Últimos posts:',
   },
+
+  policies: {
+    privacyPolicy: {
+      title: 'Política de privacidade para jairoblatt.dev',
+      textTemplate: `<p> Todas as suas informações pessoais recolhidas, serão
+      usadas para o ajudar a tornar a sua visita nesse site o mais produtiva e
+      agradável possível.</p>
+      <br />
+      <p>A garantia da confidencialidade dos dados pessoais dos usuários desse site
+        é importante para o<span class="policies__container-text--highlight">jairoblatt.dev.</span>
+      </p><br/><p>Todas as informações pessoais que usem o
+      <span class="policies__container-text--highlight">jairoblatt.dev </span>
+      serão tratadas em concordância com a Lei Nº 13.709, de 14 de Agosto de
+      2018.</p>
+    <br/>
+    <p> A informação pessoal recolhida pode incluir o seu nome, e-mail, número de telefone e/ou telemóvel, morada,
+    data de nascimento e/ou outros.</p> <br />
+    <p> O uso do <span class="policies__container-text--highlight">jairoblatt.dev </span>
+      pressupõe a aceitação deste acordo de privacidade. O site jairoblatt.dev
+      reserva-se ao direito de alterar este acordo sem aviso prévio. Deste modo,
+      recomendamos que consulte a nossa política de privacidade com regularidade
+      de forma a estar sempre atualizado.</p>`,
+    },
+
+    cookies: {
+      title: 'Os Cookies e Web Beacons',
+      textTemplate: `<p> Utilizamos cookies para armazenar informação, tais
+      como as suas preferências pessoas quando visita o nosso website. Como
+      para por exemplo guardar suas preferências de tema quando retornar ao site.</p>
+      <p> O Aplicativo usa o Google Analytics, um serviço de análise
+      prestado pelo Google, Inc. ("Google"). O Google Analytics utiliza cookies
+      para nos ajudar a analisar como os Usuários utilizam o Aplicativo. As informações
+      coletadas no cookie sobre o modo como você utiliza o Aplicativo (inclusive
+      seu endereço IP) serão transmitidas ao Google e armazenadas por ele em
+      servidores nos Estados Unidos. O Google utilizará essas informações com o
+      objetivo de avaliar o modo como você utiliza o Aplicativo, criar
+      relatórios sobre as atividades no Aplicativo e prestar outros serviços
+      relativos às atividades no Aplicativo e ao uso da Internet. O Google
+      também pode transferir essas informações a terceiros quando exigido por
+      lei ou para que tais terceiros processem as informações em nome e por
+      conta do Google. O Google não associará seu endereço IP a qualquer outro
+      dado. A utilização pelo Google desses dados é regida pelos Termos de Uso
+      do Google Analytics e pela
+      <a class="policies__container-text--highlight"> Política de Privacidade
+      do Google.</a></p> <br />
+    <p> Em adição também utilizamos publicidade de terceiros no nosso website para
+      suportar os custos de manutenção. Alguns destes publicitários, poderão
+      utilizar tecnologias como os cookies e/ou web beacons quando publicitam no
+      nosso website, o que fará com que esses publicitários (como o Google
+      através do Google AdSense) também recebam a sua informação pessoal, como o
+      endereço IP, o seu ISP, o seu browser, etc. Esta função é geralmente
+      utilizada para geotargeting (mostrar publicidade de São Paulo apenas aos
+      leitores oriundos de São Paulo por ex.) ou apresentar publicidade
+      direcionada a um tipo de utilizador (como mostrar publicidade de
+      restaurante a um utilizador que visita sites de culinária regularmente,
+      por ex.).</p><br />
+    <p> Você detém o poder de desligar os seus cookies, nas opções do seu browser.
+      No entanto, isso poderá alterar a forma como interage com o nosso website,
+      ou outros websites. Isso poderá afetar ou não permitir que faça logins em
+      sites pela internet.</p>`,
+    },
+
+    thirdPartySites: {
+      title: 'Site de terceiros',
+      textTemplate: `<p> O <span class="policies__container-text--highlight">jairoblatt.dev</span>
+      possui conexões para outros sites, que podem conter informações úteis. A
+      nossa política de privacidade não é aplicada a sites de terceiros, pelo
+      que, caso visite outro site a partir do nosso deverá ler a politica de
+      privacidade do mesmo. Política de Privacidade do Google.
+    </p><br />
+    <p>Não nos responsabilizamos pela política de privacidade ou conteúdo
+      presente nesses mesmos sites. </p>`,
+    },
+  },
 };
